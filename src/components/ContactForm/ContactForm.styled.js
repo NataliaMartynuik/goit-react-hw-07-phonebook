947f165a1402fea0c3dfd7c3f70d58c0178b30c5
NataliaMartynuik <98057592+NataliaMartynuik@users.noreply.github.com>
2022-11-28 22:58:38 +0200

    commit

--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const ContactForm = styled.form`
   border: 1px solid rgb(79, 68, 60);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   display: flex;
   flex-direction: column;
   width: 500px;
